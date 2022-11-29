21585e7399e4433187f046ed24ff559cfcf3ed4c
longvu2211 <vulongbinhduong@gmail.com>
2022-11-29 12:11:30 +0700

    Second time

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -18,9 +18,13 @@ public class MathUtilLive {
 //        long result2 = MathUtil.getFactorial(-5);
         System.out.println("expected 6! = 720; actual: " + MathUtil.getFactorial(6));
         System.out.println("expected 0! = 1; actual: " + MathUtil.getFactorial(0));
+        // Adding more codes, and uploading to the server
+        // 12:10PM 29/11/2022
+        System.out.println("expected 1! = 1; actual: " + MathUtil.getFactorial(0));
+        System.out.println("expected 3! = 6; actual: " + MathUtil.getFactorial(3));
+
         // software testing: ước lượng xem giá trị trả về của hàm gì là gì
         // expected value: 120, if expected = actual -> good program
-        
     }
-    
+
 }
